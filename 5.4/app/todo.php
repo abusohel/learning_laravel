0e35eccaac2ae7523d0bb426caf6171dfdbbea97
[abusohel] <[abusoheljnu@gmail.com]>
2018-02-08 00:48:08 +0600

    [accessor and mutator]

--- 5.4/app/todo.php
+++ 5.4/app/todo.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class todo extends Model
 {
-    //
+    // public function getBodyAttribute($value)
+    // {
+    // 	return ucfirst($value);
+    // }
+    public function getBodyAttribute($value)
+    {
+    	return $this->attributes['body']=ucfirst($value);
+    }
 }
