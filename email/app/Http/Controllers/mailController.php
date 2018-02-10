c3a4a53120bd0901b3ba0692bcb44f021099fd67
[abusohel] <[abusoheljnu@gmail.com]>
2018-02-10 14:01:14 +0600

    [update maill using html tag and mail function]

--- email/app/Http/Controllers/mailController.php
+++ email/app/Http/Controllers/mailController.php
@@ -4,15 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Mail;
+use App\Mail\sendMail;
 class mailController extends Controller
 {
     //
     public function send()
     {
-    	Mail::send(['text'=>'mail'],['name','abusohel'],function($message){
-    		$message->to('abusohelcse28@gmail.com','to sohel')->subject('test email');
-    		$message->from('abusohelcse28@gmail.com','sohel');
-    	});	
+    	Mail::send(new sendMail());	
     }
 	
 
--- email/resources/views/mail.blade.php
+++ email/resources/views/mail.blade.php
@@ -1,1 +1,12 @@
-this is a new mail
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Welcome, this is a new mail</h1>
+	
+</body>
+</html>
