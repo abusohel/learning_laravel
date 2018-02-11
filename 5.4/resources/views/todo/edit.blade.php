304d364c366166e8b63c6b8f8f7f74ce40e451b5
[abusohel] <[abusoheljnu@gmail.com]>
2018-02-11 18:51:39 +0600

    [ 2 project delete]

--- 5.4/resources/views/todo/edit.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('todo.create')
-@section('editId',$item->id)
-@section('editTitle',$item->title)
-@section('editBody',$item->body)
-
-@section('editMethod')
-	{{method_field('PUT')}}
-@endsection
--- 5.4/resources/views/todo/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.app')
-
-@section('body')
-<br>
-	<div class="col-lg-offset-4 col-lg-4">
-		<h1>{{$item->title}}</h1>
-		<p>{{$item->body}}</p>
-	</div>
-@endsection
