516af1e74467166f12bd6f98e8ff73b5b3ce1e0e
[abusohel] <[abusoheljnu@gmail.com]>
2018-02-12 00:16:19 +0600

    [factory seeder with ner seeder]

---
+++ email/email/resources/views/pagination.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pagination</title>
+</head>
+<body>
+	<div class="row">
+		<div class="container">
+			<table>
+				<thead>
+					<th>Name</th>
+					<th>Email</th>
+				</thead>
+				<tbody>
+					<td>name</td>
+					<td>email</td>
+				</tbody>
+				
+			</table>
+		</div>
+	</div>
+	
+</body>
+</html>
