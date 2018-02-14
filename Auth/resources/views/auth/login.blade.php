c653cfc9591fd38bb60cde704961586f1edd17c8
[abusohel] <[abusoheljnu@gmail.com]>
2018-02-14 22:46:06 +0600

    [email with name login instite of password]

--- Auth/resources/views/auth/login.blade.php
+++ Auth/resources/views/auth/login.blade.php
@@ -26,14 +26,14 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">name</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" required>
 
-                                @if ($errors->has('password'))
+                                @if ($errors->has('name'))
                                     <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                        <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
                             </div>
