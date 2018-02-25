a0b437ff1f498e11fdf6bba3bf42bbfe5d63b6b2
abu sohel <abusoheljnu@gmail.com>
2018-02-25 15:45:39 +0600

    file upload file showing

--- test/app/Http/Controllers/uploadController.php
+++ test/app/Http/Controllers/uploadController.php
@@ -17,10 +17,11 @@ class uploadController extends Controller
     {
     	if ($request->hasFile('image')) {
     		$request->file('image');
-    		return Storage::putFile('public/new',$request->file('image'));
+    		// return Storage::putFile('public/new',$request->file('image'));
     		// return $request->image->path();
     		// return $request->image->extension();
     		// return $request->image->store('public');
+    		return $request->image->storeAs('public','sohel.jpg');
     	}else{
     		return "no file selected.";
     	}
@@ -32,8 +33,10 @@ class uploadController extends Controller
      // return Storage::files('public');
     	// return Storage::allFiles('public');
     	// return Storage::makeDirectory('public/make');
-    	if (Storage::deleteDirectory('public/new')) {
-    		return 'deleted';
-    	}	
+    	// if (Storage::deleteDirectory('public/new')) {
+    	// 	return 'deleted';
+    	$url=Storage::url('sohel.jpg');
+    	return "<img src='".$url."'/>";
+    	// php artisan storage:link  when we neet url then we create this artisn command
      }
 }
