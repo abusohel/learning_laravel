c5c66e7c76b4f2d566d96746412137f3b39b0fb5
abu sohel <abusoheljnu@gmail.com>
2018-02-25 15:11:25 +0600

    file upload path extension put

--- test/app/Http/Controllers/uploadController.php
+++ test/app/Http/Controllers/uploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class uploadController extends Controller
 {
@@ -14,6 +15,15 @@ class uploadController extends Controller
 
     public function store(Request $request)
     {
-    	return $request->file('image');
+    	if ($request->hasFile('image')) {
+    		$request->file('image');
+    		return Storage::putFile('public',$request->file('image'));
+    		// return $request->image->path();
+    		// return $request->image->extension();
+    		// return $request->image->store('public');
+    	}else{
+    		return "no file selected.";
+    	}
+    	
     }
 }
